91537855205aaaf42a99acd7cde8d34a79c54a06
pawe9N <zerratul@wp.pl>
2017-08-26 16:37:40 +0200

    Added images and disabled submit
    as above

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 
-	$(this).searchRandomPage();
+	$('#submit').prop('disabled', true);
+    $('#search').on("keyup focus mousedown", function(){
+        $('#submit').prop('disabled', this.value == "" ? true : false);     
 
-	$("#search").keydown(function(event){
-		if(event.which == 13){
-			$('#submit').click();
+        if(event.which == 13 &&
+		   !$('#submit').attr('disabled')){
+				$('#submit').click();
 		}
-	})
+    });
+
+	$(this).searchRandomPage();
 
 	$('#submit').click(function(){
 		let title = $('#search').val();
-		$('#title').html(title);
 		$(document).searchPage(title);
 	});
 
@@ -29,51 +32,83 @@ $.fn.searchRandomPage = function () {
 	$.getJSON("https://" + language + ".wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0",
 	 function(data) {
         try{
-        	var queryRandomPage = data.query.pages;
-	        var queryRandomPageKey = Object.keys(queryRandomPage)[0];
+        	let queryRandomPage = data.query.pages;
+	        let queryRandomPageKey = Object.keys(queryRandomPage)[0];
 	        var title = queryRandomPage[queryRandomPageKey]["title"];
-             if(title != undefined){
-	        	    $('#title').html(title);
-	        	}
-	        	else{
+             if(title == undefined){
 	        		$('#title').html("Nothing");
-	        	}
+	        }
         }
         catch (err) {
-             $('#title').html(err.message);
+              console.log(err.message);
         }
         
-       $(document).searchPage(title);
+        let random = true;
+        $(document).searchPage(title, random);
    }); 
 }
 
-$.fn.searchPage = function (title) {
+$.fn.searchPage = function (title, random=false) {
 	let language = $('#language').val();
 
 	if(language == undefined){
 		language = "en";
 	}
-     $.getJSON("https://" + language + ".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + title,
+     $.getJSON("https://"+language+".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&limit=1&exintro=&explaintext=&titles="+title,
        function(data){
 	        try{
-	        	var queryPage = data.query.pages;
-	       		var queryPagesKey = Object.keys(queryPage)[0];
-	        	var wikiContent = queryPage[queryPagesKey]["extract"];
+	        	let queryPage = data.query.pages;
+	       		let queryPagesKey = Object.keys(queryPage)[0];
+	        	let wikiContent = queryPage[queryPagesKey]["extract"];
 	        	if(wikiContent != undefined &&
 	        	   wikiContent != "" &&
 	        	   wikiContent.search('From other capitalisation') < 0 && 
+	        	   wikiContent.search('From a miscapitalisation') < 0 && 
 	        	   wikiContent.search('may refer to') < 0 ){
-	        	    $('#text').html(wikiContent);
+	        	   	   $('#title').html(title);
+	        	       $('#text').html(wikiContent);
+	        	}
+	        	else if(random){
+	        	    $('#random').click();
 	        	}
 	        	else{
-	        		$('#text').html("There is nothing. You have to choose another page!");
+	        		$("#content").emptyPageOccurring();
 	        	}
 	        }
 	        catch (err) {
-	        	$('#text').html("There is nothing. You have to choose another page!");
+	        	$("#content").emptyPageOccurring();
 	            console.log(err.message);
 	        }
 	 	});
+
+
+
+     $('#search').val("").focus().takingImage(title,language);
 }
 
+$.fn.emptyPageOccurring = function(){
+	$('#title').html("Nothing");
+	$('#text').html("There is nothing. You have to choose another page!");
+	$('#text').append("<button class='random'>Random Page</button>");
+	$('.random').click(function(){
+			$(document).searchRandomPage();
+	});
+}
 
+$.fn.takingImage = function(title, language){
+	 $.getJSON("https://"+language+".wikipedia.org/w/api.php?action=query&prop=pageimages&pithumbsize=80&format=json&titles="+title,
+	 	function(data){
+	 		try{
+		 		let queryPage = data.query.pages;
+		       	let queryPagesKey = Object.keys(queryPage)[0];
+		        let thumbnail = Object.keys(queryPage[queryPagesKey])[3];
+		        let source = Object.keys(queryPage[queryPagesKey][thumbnail])[0];
+		        let image = queryPage[queryPagesKey][thumbnail][source];
+		        $('#image').css("display", "block");
+	    		$("#image").attr("src", image);
+	   		 }
+	   		 catch(err){
+	   		 	$('#image').css("display", "none");
+	   		 }
+	 	});
+}
